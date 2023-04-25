893f86b05c47d80330d161c807bd8677cc56e5b3
pradeep1331 <spradeep1331@gmail.com>
2023-04-25 21:52:11 +0530

    Initial Day1 handsOn

--- src/test/java/HandsOnDay2/AddDigit.java
+++ src/test/java/HandsOnDay2/AddDigit.java
@@ -6,6 +6,8 @@ public class AddDigit {
         lstDgt(38);
         lstDgt(0);
         lstDgt(2);
+        lstDgt(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
 
     }
 
@@ -13,11 +15,11 @@ public class AddDigit {
 
         if (num > 10) {
 
-            while (num > 10) {
+            while (num >= 10) {
                 int ans = 0;
                 while (num > 0) {
-                    ans += num % 10;
-                    num = num / 10;
+                    ans += num % 10; // 123 12 1 0
+                    num = num / 10;  //  12 1
                 }
                 num = ans;
 
--- src/test/java/HandsOnDay2/removeFirstLastOccurance.java
+++ src/test/java/HandsOnDay2/removeFirstLastOccurance.java
@@ -8,13 +8,9 @@ public class removeFirstLastOccurance {
     public static void main(String[] args) {
 
          int[] gvn = {1,2,3,4,5,1};
-
-        int[] gvn1 = {1,2,3,4,5};
-
+         int[] gvn1 = {1,2,3,4,5};
         removeOccurance(gvn,1);
         removeOccurance(gvn1,2);
-
-
     }
 
     public static void removeOccurance(int[] arr,int value){
@@ -26,7 +22,7 @@ public class removeFirstLastOccurance {
         for ( int j : arr ) {
             if(j==value) occurance++;
         }
-        System.out.println(occurance);
+//        System.out.println(occurance);
          for(int i=0;i<arr.length;i++){
              if(arr[i]==value){
                  if(firstOccurance==-1){
@@ -35,8 +31,8 @@ public class removeFirstLastOccurance {
                  lastOccurance=i;
              }
          }
-        System.out.println(firstOccurance);
-        System.out.println(lastOccurance);
+//        System.out.println(firstOccurance);
+//        System.out.println(lastOccurance);
         ArrayList<Integer> newArr = new ArrayList<Integer>();
 
 
