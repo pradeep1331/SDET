71d9fd7750e4ebb3bbb7936fd72cff65298797f9
pradeep1331 <spradeep1331@gmail.com>
2023-05-02 21:30:09 +0530

    Initial Day1 handsOn

--- src/test/java/HandsOnDay7/mergeArr.java
+++ src/test/java/HandsOnDay7/mergeArr.java
@@ -5,9 +5,6 @@ public class mergeArr {
 
         System.out.println(mrgArr("abc","pqr"));
         System.out.println(mrgArr("ab","pqrs"));
-
-
-
     }
 
     public static String mrgArr(String word1,String word2){
@@ -21,17 +18,14 @@ public class mergeArr {
               s1.append(word2.charAt(left));
               left++;
           }
-
           if(word1.length()-1>stpLen) {
-              return s1.toString() + word1.substring(stpLen+1);
+              return s1+ word1.substring(stpLen+1);
           }else if(word2.length()-1>stpLen) {
 
-              return s1.toString() + word2.substring(stpLen+1);
+              return s1 + word2.substring(stpLen+1);
           }else {
               return s1.toString();
           }
-
-
 
     }
 }
