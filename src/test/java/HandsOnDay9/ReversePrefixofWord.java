9f0355779d43a835516677d5c1b6a4ad13f1a977
pradeep1331 <spradeep1331@gmail.com>
2023-05-04 21:29:30 +0530

    Initial Day1 handsOn

---
+++ src/test/java/HandsOnDay9/ReversePrefixofWord.java
@@ -0,0 +1,30 @@
+package HandsOnDay9;
+
+public class ReversePrefixofWord {
+
+    public static void main(String[] args) {
+
+        String gvn1 = "abcdefd";
+        String gvn2 = "abcd";
+
+        System.out.println(revString(gvn1,'d'));
+        System.out.println(revString(gvn2,'z'));
+    }
+
+    public static String revString (String gvn,char c){
+
+        StringBuilder s1 = new StringBuilder(gvn);
+        int n = gvn.indexOf(c);
+        if (n<0) return gvn;
+        int left = 0,right=n;
+        while(left < right){
+             char temp = s1.charAt(left);
+             s1.setCharAt(left,s1.charAt(right));
+            s1.setCharAt(right,temp);
+            left++;
+            right--;
+
+        }
+        return s1.toString();
+    }
+}
---
+++ src/test/java/HandsOnDay9/SortArrayByParity.java
@@ -0,0 +1,45 @@
+package HandsOnDay9;
+
+
+import java.util.Arrays;
+
+public class SortArrayByParity {
+
+    public static void main(String[] args) {
+
+        int[] gvn ={3,1,2,4};
+        int[] gvn1 ={0};
+        int[] gvn2 ={3,2};
+        int[] gvn3 ={3,5,7};
+        System.out.println(Arrays.toString(sortEven(gvn)));
+        System.out.println(Arrays.toString(sortEven(gvn1)));
+        System.out.println(Arrays.toString(sortEven(gvn2)));
+        System.out.println(Arrays.toString(sortEven(gvn3)));
+    }
+
+    public static int[] sortEven(int[] arr){
+
+             int left = 0 , right = 1;
+             if(arr.length==0){
+                 return arr;
+             }
+
+             while(right<arr.length){
+                 if(arr[right] %2==0){
+                     int temp = arr[left];
+                     arr[left] = arr[right];
+                     arr[right] = temp;
+                     left++;
+                     right++;
+                 }else {
+                     right++;
+
+                 }
+
+             }
+
+
+        return arr;
+
+    }
+}
