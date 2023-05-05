01593e559078f1da8b8435645a728b69b672df63
pradeep1331 <spradeep1331@gmail.com>
2023-05-05 21:48:48 +0530

    Initial Day1 handsOn

---
+++ src/test/java/HandsOnDay10/RevString.java
@@ -0,0 +1,32 @@
+package HandsOnDay10;
+
+public class RevString {
+    public static void main(String[] args) {
+         String gvn = "Let's take LeetCode contest";
+        revStr(gvn);
+    }
+    public static void revStr(String gvn){
+        String[] arr = gvn.split(" ");
+        String output = "";
+        for (String s : arr) {
+            output = output + revgvnStr1(s) + " ";
+        }
+        System.out.println(output.trim());
+    }
+
+    public static String revgvnStr1(String gvnString){
+        StringBuilder sb = new StringBuilder(gvnString);
+        int left = 0 ,right = gvnString.length()-1;
+        while(left<right){
+            char temp =  sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left ++;
+            right--;
+        }
+        return sb.toString();
+    }
+
+
+
+}
