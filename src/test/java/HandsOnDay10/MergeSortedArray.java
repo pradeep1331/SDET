8fb20a7a96362854a3be3097617fcfc39e1cf47a
pradeep1331 <spradeep1331@gmail.com>
2023-05-05 21:57:07 +0530

    Initial Day1 handsOn

--- src/test/java/HandsOnDay10/MergeSortedArray.java
+++ src/test/java/HandsOnDay10/MergeSortedArray.java
@@ -7,23 +7,16 @@ import java.util.Arrays;
 public class MergeSortedArray {
 
     public static void main(String[] args) {
-
         int[] gvn1 = {1, 2, 3, 0, 0, 0};
         int[] gvn2 = {0, 2, 6};
-
-
         merge(gvn1, 3, gvn2, 3);
-
-
     }
-
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
 
         for (int i = 0; i < n; i++) {
             nums1[m + i] = nums2[i];
         }
-
         for (int i = 0; i < nums1.length - 1; i++) {
             for (int j = i + 1; j > 0; j--) {
                 if (nums1[j] < nums1[j - 1]) {
