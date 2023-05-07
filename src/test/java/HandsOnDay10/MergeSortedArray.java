9656ec316b8a82baaeffc0f2b9ebdf32f5428f44
pradeep1331 <spradeep1331@gmail.com>
2023-05-07 12:36:07 +0530

    Initial Day1 handsOn

--- src/test/java/HandsOnDay10/MergeSortedArray.java
+++ src/test/java/HandsOnDay10/MergeSortedArray.java
@@ -12,26 +12,34 @@ public class MergeSortedArray {
         merge(gvn1, 3, gvn2, 3);
     }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static int[] merge(int[] nums1, int m, int[] nums2, int n){
 
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
-        }
-        for (int i = 0; i < nums1.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
-                if (nums1[j] < nums1[j - 1]) {
-                    int temp = nums1[j];
-                    nums1[j] = nums1[j - 1];
-                    nums1[j - 1] = temp;
-                } else {
-                    break;
-                }
-            }
-        }
 
-        System.out.println(Arrays.toString(nums1));
+
+        return nums1;
 
     }
+
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//
+//        for (int i = 0; i < n; i++) {
+//            nums1[m + i] = nums2[i];
+//        }
+//        for (int i = 0; i < nums1.length - 1; i++) {
+//            for (int j = i + 1; j > 0; j--) {
+//                if (nums1[j] < nums1[j - 1]) {
+//                    int temp = nums1[j];
+//                    nums1[j] = nums1[j - 1];
+//                    nums1[j - 1] = temp;
+//                } else {
+//                    break;
+//                }
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(nums1));
+//
+//    }
 }
 
     ///////////////////////////////
