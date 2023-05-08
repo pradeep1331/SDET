00c82de0c7f726ba2e9459f50495a979b15ec3bc
pradeep1331 <spradeep1331@gmail.com>
2023-05-08 22:46:47 +0530

    Initial Day1 handsOn

--- src/test/java/HandsOnDay11/distinctArray.java
+++ src/test/java/HandsOnDay11/distinctArray.java
@@ -20,52 +20,33 @@ public class distinctArray {
         int[] array6 = {4, 3, 2, 1};
         System.out.println(fndDistinct(array5,array6));
 
+        int[] array7 = null;
+        int[] array8 = {4, 3, 2, 1};
+        System.out.println(fndDistinct(array7,array8));
+
 
     }
 
     public static boolean fndDistinct(int[] arr , int[] arr1) {
+        if (arr == null || arr1==null || arr ==null || arr1 == null) {
+            return false;
+        }
+        boolean flag = false;
         // Create hash maps for the arrays
         HashMap<Integer, Integer> map1 = new HashMap<>();
         HashMap<Integer, Integer> map2 = new HashMap<>();
-
         // Add array elements to hash maps
-        for (int i = 0; i < arr.length; i++) {
-            map1.put(arr[i], map2.getOrDefault(arr[i], 0) + 1);
+        for (int i : arr) {
+            map1.put(i, map2.getOrDefault(i, 0) + 1);
         }
-        for (int i = 0; i < arr1.length; i++) {
-            map2.put(arr1[i], map2.getOrDefault(arr1[i], 0) + 1);
+        for (int j : arr1) {
+            map2.put(j, map2.getOrDefault(j, 0) + 1);
         }
-
         // Compare the hash maps
-        boolean isSame = map1.equals(map2);
+         flag = map1.equals(map2);
 
-
-        return isSame;
+        return flag;
     }
-
-
-//    public static boolean fndDistinct(int[] arr , int[] arr1){
-//
-//        if (arr.length != arr1.length) {
-//        return false;
-//        }
-//
-//
-//        Map<Integer, Integer> map1 = new HashMap<>();
-//        Map<Integer, Integer> map2 = new HashMap<>();
-//
-//        for (int i = 0; i < arr.length; i++) {
-//
-//            map1.put(arr[i],map1.getOrDefault(arr[i],0)+1);
-//        }
-//
-//        for (int i = 0; i < arr1.length; i++) {
-//            map2.put(arr1[i],map1.getOrDefault(arr[i],0)+1);
-//        }
-//
-//        boolean isSame = map1.equals(map2);
-//        return isSame;
-//    }
 
 
 }
